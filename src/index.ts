459a09e3af140177c5ca24dd45ad55582be4c5da
hatakeyama-akari <hatakeyama@usedge.tech>
2021-03-17 16:58:51 +0900

    Embedding shapespark into iframe

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,26 @@
-import * as THREE from "three";
+function init() {
+    const URL_FRONT_SIDE = 'https://usedge.shapespark.com/example-room/';
+    const URL_BACK_SIDE = 'https://usedge.shapespark.com/model/';
 
-window.addEventListener("DOMContentLoaded", () => {
-  // レンダラーを作成
-  const renderer = new THREE.WebGLRenderer();
-  // レンダラーのサイズを設定
-  renderer.setSize(800, 600);
-  // canvasをbodyに追加
-  document.body.appendChild(renderer.domElement);
+    let roomUrl = URL_FRONT_SIDE;
 
-  // シーンを作成
-  const scene = new THREE.Scene();
+    let iframeElement = document.createElement('iframe');
+    iframeElement.setAttribute('src', roomUrl);
+    iframeElement.setAttribute('allow', 'gyroscope; accelerometer; xr-spatial-tracking; vr;');
+    iframeElement.setAttribute('scrolling', 'no');
+    iframeElement.setAttribute('allowfullscreen', 'true');
+    iframeElement.style.width = '100%';
+    iframeElement.style.height = '100%';
+    iframeElement.style.border = 'none';
 
-  // カメラを作成
-  const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 10000);
-  camera.position.set(0, 0, 1000);
+    document.getElementById('iframe').appendChild(iframeElement);
 
-  // 箱を作成
-  const geometry = new THREE.BoxGeometry(250, 250, 250);
-  const material = new THREE.MeshPhongMaterial({ color: 0x00bbbb });
-  const box = new THREE.Mesh(geometry, material);
-  box.position.z = -5;
-  scene.add(box);
+    document.getElementById('switch-room').onclick = () => {
+        roomUrl = roomUrl === URL_FRONT_SIDE ? URL_BACK_SIDE : URL_FRONT_SIDE;
+        iframeElement.setAttribute('src', roomUrl);
+    };
+}
 
-  // 平行光源を生成
-  const light = new THREE.DirectionalLight(0xffffff);
-  light.position.set(1, 1, 1);
-  scene.add(light);
-
-  const tick = (): void => {
-    requestAnimationFrame(tick);
-
-    box.rotation.x += 0.01;
-    box.rotation.y += 0.02;
-
-    // 描画
-    renderer.render(scene, camera);
-  };
-  tick();
-
-  console.log("Hello Three.js");
-});
+window.onload = () => {
+    init();
+};
