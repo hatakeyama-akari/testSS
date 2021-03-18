af2f0de2cdb8dbc0d0ee2b4f0d05b9d6fc37ac2e
hatakeyama-akari <hatakeyama@usedge.tech>
2021-03-18 09:54:02 +0900

    Add meeting room

--- bundle.js
+++ bundle.js
@@ -4,8 +4,10 @@ var __webpack_exports__ = {};
   !*** ./src/index.ts ***!
   \**********************/
 function init() {
-    var URL_FRONT_SIDE = 'https://usedge.shapespark.com/example-room/';
+    var URL_FRONT_SIDE = 'https://demo.shapespark.com/api-examples-autoplay/';
     var URL_BACK_SIDE = 'https://usedge.shapespark.com/model/';
+    var URL_MEETING = 'https://usedge.shapespark.com/example-room/#meeting-key=MQwo3CGLKhfxhsIV';
+    var MEETING_KEY = 'MQwo3CGLKhfxhsIV';
     var roomUrl = URL_FRONT_SIDE;
     var iframeElement = document.createElement('iframe');
     iframeElement.setAttribute('src', roomUrl);
@@ -20,6 +22,43 @@ function init() {
         roomUrl = roomUrl === URL_FRONT_SIDE ? URL_BACK_SIDE : URL_FRONT_SIDE;
         iframeElement.setAttribute('src', roomUrl);
     };
+    document.getElementById('meeting').onclick = function () {
+        var errorMassageElement = document.getElementById('meeting-key-error-message');
+        if (errorMassageElement.classList.contains('show')) {
+            errorMassageElement.classList.remove('show');
+        }
+        switchMeetingModal();
+    };
+    document.getElementById('meeting-key-submit').onclick = function () {
+        var meetingKey = document.getElementById('meeting-key-input').value;
+        var errorMassageElement = document.getElementById('meeting-key-error-message');
+        if (meetingKey === MEETING_KEY) {
+            if (errorMassageElement.classList.contains('show')) {
+                errorMassageElement.classList.remove('show');
+            }
+            switchMeetingModal();
+            iframeElement.setAttribute('allow', 'gyroscope; accelerometer; xr-spatial-tracking; vr; camera; microphone;');
+            roomUrl = URL_MEETING;
+            iframeElement.setAttribute('src', roomUrl);
+        }
+        else {
+            if (!errorMassageElement.classList.contains('show')) {
+                errorMassageElement.classList.add('show');
+            }
+        }
+    };
+    document.getElementById('meeting-key-cancel').onclick = function () {
+        switchMeetingModal();
+    };
+}
+function switchMeetingModal() {
+    var modalElement = document.getElementById('meeting-key-modal');
+    if (modalElement.classList.contains('show')) {
+        modalElement.classList.remove('show');
+    }
+    else {
+        modalElement.classList.add('show');
+    }
 }
 window.onload = function () {
     init();
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 function init() {
-    const URL_FRONT_SIDE = 'https://usedge.shapespark.com/example-room/';
+    const URL_FRONT_SIDE = 'https://demo.shapespark.com/api-examples-autoplay/';
     const URL_BACK_SIDE = 'https://usedge.shapespark.com/model/';
+
+    const URL_MEETING = 'https://usedge.shapespark.com/example-room/#meeting-key=MQwo3CGLKhfxhsIV';
+    const MEETING_KEY = 'MQwo3CGLKhfxhsIV';
 
     let roomUrl = URL_FRONT_SIDE;
 
@@ -19,6 +22,52 @@ function init() {
         roomUrl = roomUrl === URL_FRONT_SIDE ? URL_BACK_SIDE : URL_FRONT_SIDE;
         iframeElement.setAttribute('src', roomUrl);
     };
+
+    document.getElementById('meeting').onclick = () => {
+        let errorMassageElement = document.getElementById('meeting-key-error-message');
+        if (errorMassageElement.classList.contains('show')) {
+            errorMassageElement.classList.remove('show');
+        }
+
+        switchMeetingModal();
+    };
+
+    document.getElementById('meeting-key-submit').onclick = () => {
+        let meetingKey = (<HTMLInputElement>document.getElementById('meeting-key-input')).value;
+        let errorMassageElement = document.getElementById('meeting-key-error-message');
+
+        if (meetingKey === MEETING_KEY) {
+            if (errorMassageElement.classList.contains('show')) {
+                errorMassageElement.classList.remove('show');
+            }
+
+            switchMeetingModal();
+
+            iframeElement.setAttribute(
+                'allow',
+                'gyroscope; accelerometer; xr-spatial-tracking; vr; camera; microphone;'
+            );
+            roomUrl = URL_MEETING;
+            iframeElement.setAttribute('src', roomUrl);
+        } else {
+            if (!errorMassageElement.classList.contains('show')) {
+                errorMassageElement.classList.add('show');
+            }
+        }
+    };
+
+    document.getElementById('meeting-key-cancel').onclick = () => {
+        switchMeetingModal();
+    };
+}
+
+function switchMeetingModal() {
+    let modalElement = document.getElementById('meeting-key-modal');
+    if (modalElement.classList.contains('show')) {
+        modalElement.classList.remove('show');
+    } else {
+        modalElement.classList.add('show');
+    }
 }
 
 window.onload = () => {
